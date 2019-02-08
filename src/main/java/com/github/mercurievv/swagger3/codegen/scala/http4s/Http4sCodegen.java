bb57c59ccfeb6bbf8e15c8d0dd6de11d61dd5970
MercurieVV <mercurievv@gmail.com>
2019-02-09 01:24:04 +0200

    Do improvements

--- src/main/java/com/github/mercurievv/swagger3/codegen/scala/http4s/Http4sCodegen.java
+++ src/main/java/com/github/mercurievv/swagger3/codegen/scala/http4s/Http4sCodegen.java
@@ -1,14 +1,14 @@
 package com.github.mercurievv.swagger3.codegen.scala.http4s;
 
-import io.swagger.codegen.v3.CodegenModel;
 import io.swagger.codegen.v3.CodegenOperation;
 import io.swagger.codegen.v3.CodegenParameter;
+import io.swagger.codegen.v3.CodegenProperty;
 import io.swagger.codegen.v3.CodegenType;
-import io.swagger.codegen.v3.generators.handlebars.ExtensionHelper;
 import io.swagger.codegen.v3.generators.scala.AkkaHttpServerCodegen;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,51 +47,184 @@ public class Http4sCodegen extends AkkaHttpServerCodegen {
         return "Generates an http4s http server in scala";
     }
 
-/*
     @Override
-    public void processModelEnums(Map<String, Object> objs) {
-        System.out.println("objs = " + ((CodegenModel) ((Map<String, Object>) objs.get("models")).get("model")).allowableValues);
-        super.processModelEnums(objs);
+    public void processOpts() {
+        super.processOpts();
+
+        importMapping.clear();
+
+//        instantiationTypes.put("array", "List");
+        instantiationTypes.put("map", "Map");
+
+//        typeMapping.put("FeatureTestResult", "FeatureTestResult.FeatureTestResult");
+        importMapping.remove("ApiModelProperty");
+        importMapping.remove("ApiModel");
+
+        importMapping.remove("List");
+
+        importMapping.put("JsonValue", "scala.Int");
+
+        typeMapping.put("array", "List");
+        typeMapping.put("map", "Map");
+
+        typeMapping.put("integer", "Int");
+        importMapping.put("Int", "scala.Int");
+
+        typeMapping.put("date", "LocalDate");
+        importMapping.put("LocalDate", "java.time.LocalDate");
+
+        typeMapping.put("DateTime", "ZonedDateTime");
+        importMapping.put("ZonedDateTime", "java.time.ZonedDateTime");
+
+        importMapping.put("OffsetDateTime", "java.time.OffsetDateTime");
+
+        importMapping.put("BigDecimal", "scala.BigDecimal");
     }
-*/
-public Map<String, Object> postProcessModels(Map<String, Object> objs) {
-    List<Map<String, String>> recursiveImports = (List<Map<String, String>>)objs.get("imports");
-    if (recursiveImports == null) {
-        return objs;
-    } else {
-        ListIterator<Map<String, String>> listIterator = recursiveImports.listIterator();
 
-        while(listIterator.hasNext()) {
-            String _import = (listIterator.next()).get("import");
-            if (this.importMapping.containsKey(_import)) {
-                Map<String, String> newImportMap = new HashMap<>();
-                newImportMap.put("import", this.importMapping.get(_import));
-                listIterator.add(newImportMap);
-            }
+    @Override
+    public CodegenParameter fromRequestBody(RequestBody body, String name, Schema schema, Map<String, Schema> schemas, Set<String> imports) {
+        final CodegenParameter codegenParameter = super.fromRequestBody(body, name, schema, schemas, imports);
+        if (body.getExtensions() != null && body.getExtensions().get("x-varname") != null) {
+            final String bodyName = (String) body.getExtensions().get("x-varname");
+            codegenParameter.baseName = bodyName;
+            codegenParameter.paramName = bodyName;
         }
+        return codegenParameter;
+    }
 
+    public String getSchemaType(Schema schema) {
+        String schemaType = super.getSchemaType(schema);
+        schemaType = this.getAlias(schemaType);
+        if (this.typeMapping.containsKey(schemaType)) {
+            return this.typeMapping.get(schemaType);
+        } else {
+            if (null == schemaType && schema.getName() != null) {
+                LOGGER.warn("No Type defined for Property " + schema.getName());
+            }
+
+            return this.toModelName(schemaType);
+        }
+    }
+
+    public String toModelName(String name) {
+        return camelize(this.sanitizeName(name));
+    }
+
+    public String toEnumName(CodegenProperty property) {
+        return this.sanitizeName(camelize(property.name)) + "Enum";
+    }
+
+    public Map<String, Object> postProcessModels(Map<String, Object> objs) {
+/*        List<Map<String, String>> recursiveImports = (List<Map<String, String>>) objs.get("imports");
+        if (recursiveImports == null) {
+            return objs;
+        } else {
+            ListIterator<Map<String, String>> listIterator = recursiveImports.listIterator();
+
+            while (listIterator.hasNext()) {
+                String _import = (listIterator.next()).get("import");
+                if (this.importMapping.containsKey(_import)) {
+                    Map<String, String> newImportMap = new HashMap<>();
+                    newImportMap.put("import", this.importMapping.get(_import));
+                    listIterator.add(newImportMap);
+                }
+            }
+
+            return this.postProcessModelsEnum(objs);
+        }*/
+        super.postProcessModels(objs);
         return this.postProcessModelsEnum(objs);
+
     }
-}
 
-    public Map<String, Object> postProcessModelsEnum(Map<String, Object> objs) {
-        objs = super.postProcessModelsEnum(objs);
-        List<Map<String, String>> imports = (List<Map<String, String>>)objs.get("imports");
-        List<Object> models = (List)objs.get("models");
+    @Override
+    public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+        Map<String, Object> stringObjectMap = super.postProcessOperations(objs);
 
-        for (Object _mo : models) {
-            Map<String, Object> mo = (Map) _mo;
-            CodegenModel cm = (CodegenModel) mo.get("model");
-            boolean isEnum = ExtensionHelper.getBooleanValue(cm, "x-is-enum");
-            if (Boolean.TRUE.equals(isEnum) && cm.allowableValues != null) {
-//                cm.imports.add(this.importMapping.get("JsonValue"));
-                Map<String, String> item = new HashMap<>();
-                item.put("import", this.importMapping.get("JsonValue"));
-                imports.add(item);
+        Map<String, Object> operations = (Map<String, Object>) stringObjectMap.get("operations");
+        if (operations == null) {
+            return stringObjectMap;
+        }
+        List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+        for (CodegenOperation operation : ops) {
+            if (operation.returnType == null) {
+                operation.returnType = "Unit";
+            } /*else if (operation.returnType.equals("Object")) {
+                operation.returnType = "Unit";
+            } else if (operation.returnType.equals("Unit")) {
+                operation.returnType = "Unit";
+            }*/ else if (operation.returnType.startsWith("List")) {
+                String rt = operation.returnType;
+                int end = rt.lastIndexOf("]");
+                if (end > 0) {
+                    operation.returnType = rt.substring("List[".length(), end).trim();
+                    operation.returnContainer = "List";
+                    String varName = operation.returnType.substring(0, 1).toLowerCase() + operation.returnType.substring(1) + "s";
+                    operation.vendorExtensions.put("returnTypeVar", toVarName(varName));
+                }
             }
         }
+        return stringObjectMap;
+    }
 
-        return objs;
+    @Override
+    public String toEnumVarName(String value, String datatype) {
+        String varName = toEnumVarNameNoCasing(value, datatype);
+        if (varName.equals("new")) varName = "`new`";
+        return varName;
+    }
+
+
+    @Override
+    public CodegenProperty fromProperty(String name, Schema p) {
+        CodegenProperty codegenProperty = super.fromProperty(name, p);
+        List<String> predicates = new ArrayList<>();
+        if (codegenProperty.minimum != null) {
+            predicates.add("");
+            if (codegenProperty.minimum.equals("0")) {
+                predicates.add("NonNegative");
+                codegenProperty.minimum = null;
+            }
+        }
+        if (codegenProperty.maximum != null) {
+            predicates.add("");
+            if (codegenProperty.maximum.equals("0")) {
+                predicates.add("NonPositive");
+                codegenProperty.maximum = null;
+            }
+        }
+        if (p.getFormat() != null) {
+            if (p.getFormat().startsWith(NEWTYPE_PREFIX)) {
+                toModelImport("import shapeless.tag.@@");
+                toApiImport("import shapeless.tag.@@");
+                toApiImport("import shapeless.tag");
+
+                final String newtypeName = p.getFormat().substring(NEWTYPE_PREFIX.length());
+                codegenProperty.vendorExtensions.put("newtypeName", newtypeName);
+            }
+        }
+        if (p.getFormat() != null) {
+            if (p.getFormat().startsWith(TYPE_PREFIX)) {
+                final String newtypeName = p.getFormat().substring(TYPE_PREFIX.length());
+                codegenProperty.vendorExtensions.put("typeName", newtypeName);
+            }
+        }
+        if (!predicates.isEmpty()) {
+            codegenProperty.vendorExtensions.put("predicates", predicates.stream().collect(Collectors.joining(" ")));
+            toModelImport("import eu.timepit.refined._");
+            toModelImport("import eu.timepit.refined.api.Refined");
+            toModelImport("import eu.timepit.refined.auto._");
+            toModelImport("import eu.timepit.refined.numeric._");
+            toModelImport("import co.nextwireless.newtype.tags._");
+            toApiImport("import eu.timepit.refined._");
+            toApiImport("import eu.timepit.refined.api.Refined");
+            toApiImport("import eu.timepit.refined.auto._");
+            toApiImport("import eu.timepit.refined.numeric._");
+            toApiImport("import co.nextwireless.newtype.tags._");
+        }
+//        if (codegenProperty.isEnum)
+//            enumImports.add(codegenProperty.datatypeWithEnum);
+        return codegenProperty;
     }
 
 
@@ -99,23 +232,30 @@ public Map<String, Object> postProcessModels(Map<String, Object> objs) {
     public CodegenOperation fromOperation(String ppath, String httpMethod, Operation operation, Map<String, Schema> schemas, OpenAPI openAPI) {
         CodegenOperation op = super.fromOperation(ppath, httpMethod, operation, schemas, openAPI);
 //        op.vendorExtensions.put("x-enumImports", enumImports);//.stream().collect(Collectors.joining("", "import " + modelPackage + ".", "\\n")));
+        useCustomTypes(op);
 
+        http4sCode(operation, op);
+
+        op.allParams.sort(Comparator.comparingInt(this::order));
+
+        op.allParams.forEach(this::addTypesafityToParam);
+//        addHasMore((List)op.bodyParams);
+//        addHasMore((List)op.pathParams);
+//        addHasMore((List)op.queryParams);
+//        addHasMore((List)op.headerParams);
+//        addHasMore((List)op.formParams);
+//        addHasMore((List)op.requiredParams);
+        addHasMore((List) op.allParams);
+
+        return op;
+    }
+
+    private void http4sCode(Operation operation, CodegenOperation op) {
         List<String> path = pathToHttp4sRoute(op.path);
 
         List<String> type = pathToClientType(op.path, op.pathParams);
         String pathStringClient = path.stream().filter(s -> !s.isEmpty()).collect(Collectors.joining(" / "));
         String pathStringServer = processParams(operation, pathStringClient, op.queryParams);
-        op.allParams
-                .forEach(p -> {
-                    System.out.println("p.dataFormat = " + p.paramName + " " + p.dataFormat);
-                    p.datatypeWithEnum = p.dataType;
-                    if (p.dataFormat != null) {
-                        if (p.dataFormat.startsWith(TYPE_PREFIX)) {
-                            final String newtypeName = p.dataFormat.substring(TYPE_PREFIX.length());
-                            p.vendorExtensions.put("typeName", newtypeName);
-                        }
-                    }
-                });
 //        String pathStringClient = processParamsClient(pathStringClient, op.queryParams);
 //        pathStringServer = processParams(operation, pathStringServer, op.headerParams);
 
@@ -151,14 +291,65 @@ public Map<String, Object> postProcessModels(Map<String, Object> objs) {
         op.vendorExtensions.put("x-routeType", pathStringServer);
 
         op.vendorExtensions.put("x-routeTypeClient", pathStringClient);
-
-        op.allParams.forEach(this::addTypesafityToParam);
-
-        return op;
     }
 
+    private void useCustomTypes(CodegenOperation op) {
+        op.allParams
+                .forEach(p -> {
+                    p.datatypeWithEnum = p.dataType;
+                    if (p.dataFormat != null) {
+                        if (p.dataFormat.startsWith(TYPE_PREFIX)) {
+                            final String newtypeName = p.dataFormat.substring(TYPE_PREFIX.length());
+                            p.vendorExtensions.put("typeName", newtypeName);
+                        }
+                    }
+                });
+    }
 
+    private int order(CodegenParameter codegenParameter) {
+        if (codegenParameter.getIsHeaderParam())
+            return 1;
+        if (codegenParameter.getIsCookieParam())
+            return 2;
+        if (codegenParameter.getIsPathParam())
+            return 3;
+        if (codegenParameter.getIsQueryParam())
+            return 4;
+        if (codegenParameter.getIsFormParam())
+            return 5;
+        if (codegenParameter.getIsBodyParam())
+            return 6;
+        return 7;
+    }
 
+    private String toEnumVarNameNoCasing(String value, String datatype) {
+        if (value.length() == 0) {
+            return "EMPTY";
+        }
+
+        // for symbol, e.g. $, #
+        if (getSymbolName(value) != null) {
+            return getSymbolName(value);
+        }
+
+        // number
+        if ("Integer".equals(datatype) || "Long".equals(datatype) ||
+                "Float".equals(datatype) || "Double".equals(datatype)) {
+            String varName = "NUMBER_" + value;
+            varName = varName.replaceAll("-", "MINUS_");
+            varName = varName.replaceAll("\\+", "PLUS_");
+            varName = varName.replaceAll("\\.", "_DOT_");
+            return varName;
+        }
+
+        // string
+        String var = value.replaceAll("\\W+", "_");
+        if (var.matches("\\d.*")) {
+            return "_" + var;
+        } else {
+            return var;
+        }
+    }
 
     private String processParams(Operation operation, String pathString, final List<CodegenParameter> params) {
         // Query parameters appended to routes
@@ -171,6 +362,18 @@ public Map<String, Object> postProcessModels(Map<String, Object> objs) {
             pathString = pathString + queryParams;
         }
         return pathString;
+    }
+
+    private static List<CodegenParameter> addHasMore(List<CodegenParameter> objs) {
+        if (objs != null) {
+            for (int i = 0; i < objs.size(); ++i) {
+                objs.get(i).secondaryParam = i > 0;
+                objs.get(i).getVendorExtensions().put("x-has-more", i < objs.size() - 1);
+                System.out.println("i = " + i + " " + objs.get(i).secondaryParam + " " + (objs.size() - 1) + " " + objs.get(i).paramName);
+            }
+        }
+
+        return objs;
     }
 
     private void addTypesafityToParam(CodegenParameter cp) {
